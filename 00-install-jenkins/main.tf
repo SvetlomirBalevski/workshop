7d6c284d69350e9a0a9275fad44128989b2ef50d
Svet <s.balevski@gmail.com>
2023-03-09 21:58:15 +0200

    Fix typo

--- 00-install-jenkins/main.tf
+++ 00-install-jenkins/main.tf
@@ -20,7 +20,7 @@ module "vpc" {
 }
 
 module "security_groups" {
-  source = "./modules/securty_groups"
+  source = "./modules/security_groups"
   vpc_id = module.vpc.vpc_id
   my_ip  = var.my_ip
 }
